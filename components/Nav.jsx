1cfbc9ec6a323ded84cd4e4599a725b36eb7a3f3
GorgeousSuit <gorgeoussuit573@gmail.com>
2023-09-12 19:46:23 +0300

    Trying clamp

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Nav = () => {
     return (
         <section>
-            <div className="hover:[&>*]:underline w-[calc(100%-144px)] flex mx-[72px] py-[4px] justify-between absolute bottom-[100px] left-[0px] ">
+            <div className="hover:[&>*]:underline w-[calc(100%-7.5vw)] flex mx-[3.75vw] py-[4px] justify-between absolute bottom-[100px] ">
                 <Link href="/services">Services</Link>
                 <Link href="/portfolio">Portfolio</Link>
                 <Link href="/contacts">Contacts</Link>
