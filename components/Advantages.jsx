b50df814dda376e613722d19792265daf1dfbef9
GorgeousSuit <gorgeoussuit573@gmail.com>
2023-09-13 17:51:17 +0300

    Fixed Advantages

--- components/Advantages.jsx
+++ components/Advantages.jsx
@@ -21,88 +21,93 @@ const Advantages = () => {
                     </h2>
                 </div>
             </div>
-            <div className="mt-[72px] sm:mt-[120px] mb-[96px] sm:mb-[128px] max-sm:space-y-[64px] max-sm:mx-auto max-sm:w-[fit-content]">
-                <div className="">
-                    <p className="mb-[24px] sm:mb-[32px] font-normal text-[20px] sm:text-[24px] leading-[24px] sm:leading-[28.8px]">
-                        01
-                    </p>
-                    <p className="mb-[20px] sm:mb-[24px] font-normal text-[24px] sm:text-[40px] leading-[28.8px] sm:leading-[48px]">
-                        We work with goals
-                        <br className="hidden sm:block" /> and
-                        <br className="sm:hidden" /> challenges
-                    </p>
-                    <p className="font-normal leading-[22.4px] text-[#545454]">
-                        If you have a goal, we will find ways to
-                        <br className="sm:hidden" /> help you{' '}
-                        <br className="hidden sm:block" />
-                        achieve it through digital
-                        <br className="sm:hidden" /> channels.
-                    </p>
-                </div>
-                <div className="sm:flex sm:justify-end sm:mr-[220px] sm:max-xl:mt[15px] lg:mt-[-44px]">
+            <div className="sm:mx-auto sm:max-w-[1400px]">
+                <div className=" mt-[72px] sm:mt-[120px] mb-[96px] sm:mb-[128px] max-sm:space-y-[64px] max-sm:mx-auto sm:w-full">
                     <div className="">
                         <p className="mb-[24px] sm:mb-[32px] font-normal text-[20px] sm:text-[24px] leading-[24px] sm:leading-[28.8px]">
-                            02
+                            01
                         </p>
                         <p className="mb-[20px] sm:mb-[24px] font-normal text-[24px] sm:text-[40px] leading-[28.8px] sm:leading-[48px]">
-                            A comprehensive <br className="hidden sm:block" />{' '}
-                            study of
-                            <br className="sm:hidden" /> your{' '}
-                            <br className="hidden sm:block" /> product
+                            We work with goals
+                            <br className="hidden sm:block" /> and
+                            <br className="sm:hidden" /> challenges
                         </p>
                         <p className="font-normal leading-[22.4px] text-[#545454]">
-                            The better we know your product, its
-                            <br className="sm:hidden" /> benefits, and{' '}
-                            <br className="hidden sm:block" /> the customers who
-                            buy it,
-                            <br className="sm:hidden" /> the better we will
-                            build a <br className="hidden sm:block" />
-                            strategy for its
-                            <br className="sm:hidden" /> presentation in
-                            digital.
+                            If you have a goal, we will find ways to
+                            <br className="sm:hidden" /> help you{' '}
+                            <br className="hidden sm:block" />
+                            achieve it through digital
+                            <br className="sm:hidden" /> channels.
                         </p>
                     </div>
-                </div>
-                <div className="sm:ml-[110px] sm:max-xl:mt[15px] lg:mt-[-24px]">
-                    <p className="mb-[24px] sm:mb-[32px] font-normal text-[20px] sm:text-[24px] leading-[24px] sm:leading-[28.8px]">
-                        03
-                    </p>
-                    <p className="mb-[20px] sm:mb-[24px] font-normal text-[24px] sm:text-[40px] leading-[28.8px] sm:leading-[48px]">
-                        Market and <br className="hidden sm:block" /> competitor
-                        <br className="sm:hidden" />
-                        analysis
-                    </p>
-                    <p className="font-normal leading-[22.4px] text-[#545454]">
-                        We carefully study the market you operate
-                        <br className="sm:hidden" /> in and{' '}
-                        <br className="hidden sm:block" /> who you compete with.
-                        To offer
-                        <br className="sm:hidden" /> solutions that will{' '}
-                        <br className="hidden sm:block" /> make you better than
-                        <br className="sm:hidden" />
-                        the rest.
-                    </p>
-                </div>
-                <div className="sm:flex sm:justify-end sm:max-xl:mt[15px] lg:mt-[-66px]">
-                    <div className="">
+                    <div className="sm:flex sm:justify-end sm:mr-[220px] sm:max-xl:mt[15px] lg:mt-[-44px]">
+                        <div className="">
+                            <p className="mb-[24px] sm:mb-[32px] font-normal text-[20px] sm:text-[24px] leading-[24px] sm:leading-[28.8px]">
+                                02
+                            </p>
+                            <p className="mb-[20px] sm:mb-[24px] font-normal text-[24px] sm:text-[40px] leading-[28.8px] sm:leading-[48px]">
+                                A comprehensive{' '}
+                                <br className="hidden sm:block" /> study of
+                                <br className="sm:hidden" /> your{' '}
+                                <br className="hidden sm:block" /> product
+                            </p>
+                            <p className="font-normal leading-[22.4px] text-[#545454]">
+                                The better we know your product, its
+                                <br className="sm:hidden" /> benefits, and{' '}
+                                <br className="hidden sm:block" /> the customers
+                                who buy it,
+                                <br className="sm:hidden" /> the better we will
+                                build a <br className="hidden sm:block" />
+                                strategy for its
+                                <br className="sm:hidden" /> presentation in
+                                digital.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="sm:ml-[110px] sm:max-xl:mt[15px] lg:mt-[-24px]">
                         <p className="mb-[24px] sm:mb-[32px] font-normal text-[20px] sm:text-[24px] leading-[24px] sm:leading-[28.8px]">
-                            04
+                            03
                         </p>
                         <p className="mb-[20px] sm:mb-[24px] font-normal text-[24px] sm:text-[40px] leading-[28.8px] sm:leading-[48px]">
-                            We are partners for{' '}
-                            <br className="hidden sm:block" /> your
-                            <br className="sm:hidden" /> brand
+                            Market and <br className="hidden sm:block" />{' '}
+                            competitor
+                            <br className="sm:hidden" />
+                            analysis
                         </p>
                         <p className="font-normal leading-[22.4px] text-[#545454]">
-                            Having started working with your
-                            <br className="sm:hidden" /> company, we will{' '}
-                            <br className="hidden sm:block" /> always be
-                            interested in
-                            <br className="sm:hidden" /> its further development
-                            and <br className="hidden sm:block" /> will be happy
-                            <br className="sm:hidden" /> to share our expertise
-                            to help you grow.
+                            We carefully study the market you operate
+                            <br className="sm:hidden" /> in and{' '}
+                            <br className="hidden sm:block" /> who you compete
+                            with. To offer
+                            <br className="sm:hidden" /> solutions that will{' '}
+                            <br className="hidden sm:block" /> make you better
+                            than
+                            <br className="sm:hidden" />
+                            the rest.
                         </p>
+                    </div>
+                    <div className="sm:flex sm:justify-end sm:max-xl:mt[15px] lg:mt-[-66px]">
+                        <div className="">
+                            <p className="mb-[24px] sm:mb-[32px] font-normal text-[20px] sm:text-[24px] leading-[24px] sm:leading-[28.8px]">
+                                04
+                            </p>
+                            <p className="mb-[20px] sm:mb-[24px] font-normal text-[24px] sm:text-[40px] leading-[28.8px] sm:leading-[48px]">
+                                We are partners for{' '}
+                                <br className="hidden sm:block" /> your
+                                <br className="sm:hidden" /> brand
+                            </p>
+                            <p className="font-normal leading-[22.4px] text-[#545454]">
+                                Having started working with your
+                                <br className="sm:hidden" /> company, we will{' '}
+                                <br className="hidden sm:block" /> always be
+                                interested in
+                                <br className="sm:hidden" /> its further
+                                development and{' '}
+                                <br className="hidden sm:block" /> will be happy
+                                <br className="sm:hidden" /> to share our
+                                expertise to help you grow.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
