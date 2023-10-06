fbe843c07d26601f6b2233b9fd084d868ab93f41
Vyacheslav <gorgeoussuit573@gmail.com>
2023-10-06 15:43:44 +0300

    Trying to fix deploy issues x5

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,12 +1,8 @@
-import Image from "next/image";
-import Logo from "/public/assets/images/logo-lg.svg"
-
 const Loading = () => {
     const open = true;
 
     return (
         <section className="bg-black w-full h-[100vh] flex-center">
-            <Logo className="animate-pulse"/>
         </section>
     );
 };
