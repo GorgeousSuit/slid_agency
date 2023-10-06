9772a02742450ee9bc14b8076993e647e99a3058
Vyacheslav <gorgeoussuit573@gmail.com>
2023-10-06 15:22:56 +0300

    Fixed Header Part

--- app/testing/page.jsx
+++ app/testing/page.jsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import HeaderPart from "@components/HeaderPart";
 
 const page = () => {
     return (
         <section className="bg-black">
-            <HeaderPart />
         </section>
     );
 };
